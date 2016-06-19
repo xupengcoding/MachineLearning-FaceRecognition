3a57e85c373e80afec02012ba196682eed7eaf2e
unknown <>
2016-06-19 17:10:01 +0800

    完善验证网络

--- face_validation_kearsnet.py
+++ face_validation_kearsnet.py
@@ -16,8 +16,8 @@ def face_valid_net():
     keras_model = Graph()
     #input 2268 dim
     #two input layer
-    keras_model.add_input(name="input1", input_shape=(2268,))
-    keras_model.add_input(name="input2", input_shape=(2268,))
+    keras_model.add_input(name="input1", input_shape=(2622,))
+    keras_model.add_input(name="input2", input_shape=(2622,))
 
     #two dense layer
     keras_model.add_node(layer=Dense(1000, activation='relu'), name='dense1', input='input1')
@@ -30,3 +30,7 @@ def face_valid_net():
     keras_model.add_node(layer=Dense(2, activation='softmax'), name='dens4', input='dense3')
     keras_model.add_output('output1', input='dense4')
     #add soft max  or min_max or maxpooling
+    keras_model.compile('adadelta', {'output', 'categorical_crossentropy'})
+
+    history = graph.fit({'input1':X_train, 'input2':X2_train, 'output':y_train}, nb_epoch=10)  
+predictions = graph.predict({'input1':X_test, 'input2':X2_test}) # {'output':...} 
